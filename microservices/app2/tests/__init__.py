1c5e6a020463b56da3ebfef77d6ee3264731841a
Daniel Perrefort <djperrefort@gmail.com>
2023-10-12 11:37:39 -0400

    Adds demo microservices

---
+++ microservices/app2/tests/__init__.py
@@ -0,0 +1,10 @@
+"""Example tests for application 2"""
+
+from unittest import TestCase
+
+
+class ExampleTest(TestCase):
+    """This is a placeholder test class"""
+
+    def test_example(self) -> None:
+        """This is an example test that will always pass"""
---
+++ microservices/app3/main.py
@@ -0,0 +1,3 @@
+"""Dummy source code for a microservice application"""
+
+print('Hello World - This is application 3')
